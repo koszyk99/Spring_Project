1b2da2dd778f7337898caf5f769ee254a9a3b2c0
koszyk99 <krzysztof_koszyk@interia.eu>
2026-09-05 00:38:31 +0200

    feat: curl -X POST works

--- task-manager/demo/src/main/java/com/krzysztof/demo/TaskController.java
+++ task-manager/demo/src/main/java/com/krzysztof/demo/TaskController.java
@@ -3,18 +3,25 @@ package com.krzysztof.demo;
 import java.util.List;
 import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // HTTP request class
 @RestController
 public class TaskController {
 
+    private List<Task> tasks = new ArrayList<>();
+
     // a method handling GET requests at a given path
     @GetMapping("/task2")
     public List<Task> getTask2() {
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task ("Buy water", false));
-        tasks.add(new Task ("Buy juice", true));
         return tasks;
+    }
+
+    @PostMapping("/task2")
+    public Task addTask(@RequestBody Task task) {
+        tasks.add(task);
+        return task;
     }
 }
