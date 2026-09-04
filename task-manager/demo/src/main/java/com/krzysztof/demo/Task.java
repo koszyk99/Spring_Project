830f957e59617b7028b82134c1efa4558c7f0356
koszyk99 <krzysztof_koszyk@interia.eu>
2026-09-05 00:57:01 +0200

    feat: add hibernate/jpa integration with task entity and repository

--- task-manager/demo/src/main/java/com/krzysztof/demo/Task.java
+++ task-manager/demo/src/main/java/com/krzysztof/demo/Task.java
@@ -1,12 +1,34 @@
 package com.krzysztof.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity 
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private boolean done;
+
+    public Task() {
+    }
 
     public Task(String name, boolean done) {
         this.name = name;
         this.done = done;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
--- task-manager/demo/src/main/java/com/krzysztof/demo/TaskController.java
+++ task-manager/demo/src/main/java/com/krzysztof/demo/TaskController.java
@@ -1,7 +1,8 @@
 package com.krzysztof.demo;
 
 import java.util.List;
-import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskController {
 
-    private List<Task> tasks = new ArrayList<>();
+    @Autowired
+    private TaskRepository taskRepository;
 
     // a method handling GET requests at a given path
     @GetMapping("/task2")
     public List<Task> getTask2() {
-        return tasks;
+        return taskRepository.findAll();
     }
 
     @PostMapping("/task2")
     public Task addTask(@RequestBody Task task) {
-        tasks.add(task);
-        return task;
+        return taskRepository.save(task);
     }
 }
