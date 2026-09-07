642f211d4f84887351545a1ca957f96d8f5c373b
koszyk99 <krzysztof_koszyk@interia.eu>
2026-09-07 14:54:48 +0200

    feat: add full CRUD operations to TaskController

--- task-manager/demo/src/main/java/com/krzysztof/demo/TaskController.java
+++ task-manager/demo/src/main/java/com/krzysztof/demo/TaskController.java
@@ -3,8 +3,11 @@ package com.krzysztof.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +18,30 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
-    // a method handling GET requests at a given path
+    // Read (GET)
     @GetMapping("/task2")
     public List<Task> getTask2() {
         return taskRepository.findAll();
     }
 
+    // Create (POST)
     @PostMapping("/task2")
     public Task addTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    // DELETE
+    @DeleteMapping("/task2/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    // Update (PUT)
+    @PutMapping("/task2/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updateTask) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setName(updateTask.getName());
+        task.setDone(updateTask.isDone());
         return taskRepository.save(task);
     }
 }
